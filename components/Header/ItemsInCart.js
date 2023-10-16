b889f0cd3a80c38dfdafe6d356045d17947ca0b8
Siarhei Prakapovich <prokser27@icloud.com>
2023-10-17 00:01:22 +0300

    fix: fix display of number of items in cart in header

--- components/Header/ItemsInCart.js
+++ components/Header/ItemsInCart.js
@@ -4,11 +4,13 @@ import React from 'react';
 import useStore from '../../controller/store/store';
 
 export default function ItemsInCart() {
-  const itemsCount = useStore((state) => state.cart.size);
+  const { cart } = useStore();
+  const values = [...cart.values()];
+  const cartSize = values.reduce((sum, current) => (sum + current.quantity), 0);
 
   return (
     <>
-      {itemsCount !== 0 && <div className="absolute rounded-full grid place-items-center top-[-.75rem] right-[-1rem] bg-black text-white text-sm h-5 w-5">{itemsCount}</div>}
+      {cart.size !== 0 && <div className="absolute rounded-full grid place-items-center top-[-.75rem] right-[-1rem] bg-black text-white text-sm h-5 w-5">{cartSize <= 9 ? cartSize : '9+'}</div>}
     </>
   );
 }
