87d868e34b309c4656cda97f513c0e13e6c860cd
Siarhei Prakapovich <prokser27@icloud.com>
2023-10-21 22:40:26 +0300

    feat: add placeholder text if nothing in favorites

--- app/favorites/FavCards/FavCards.js
+++ app/favorites/FavCards/FavCards.js
@@ -25,19 +25,24 @@ export default function FavCards() {
     fetchData();
   }, [favorites]);
   return (
-    <div className="w-full grid gap-10 grid-cols-4 self-center py-5">
-      {
-        data.map((item) => (
-          <ItemCard
-            key={item.title}
-            id={item.id}
-            img={item.image}
-            price={item.price}
-            title={item.title}
-            category={item.category}
-          />
-        ))
-      }
-    </div>
+    <>
+      {favorites.size > 0
+        ? (
+          <div className="w-full grid gap-10 grid-cols-4 self-center py-5">
+            {data.map((item) => (
+              <ItemCard
+                key={item.title}
+                id={item.id}
+                img={item.image}
+                price={item.price}
+                title={item.title}
+                category={item.category}
+              />
+            ))}
+          </div>
+        )
+        : <p className="text-icon-blue self-center">Nothing in favorites</p>}
+    </>
+
   );
 }
--- app/favorites/page.js
+++ app/favorites/page.js
@@ -9,7 +9,7 @@ export default function Favorites() {
         itemCategory="Favorites"
       />
       <main className="flex min-h w-full relative flex-col items-center justify-center p-5 mt-3">
-        <div className="w-full max-w-[1440px] flex flex-col gap-4">
+        <div className="w-full max-w-[1440px] min-h-[666px] flex flex-col gap-4">
           <h1 className="text-2xl font-bold">My favorites</h1>
           <FavCards />
         </div>
