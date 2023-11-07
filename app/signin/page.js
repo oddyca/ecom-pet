c21049a8525c1c1bbc5e673f20e91663e7f3f882
Siarhei Prakapovich <prokser27@icloud.com>
2023-11-07 20:24:17 +0300

    refactor: show loading spinner within login button while fetching

--- app/signin/page.js
+++ app/signin/page.js
@@ -5,7 +5,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import {
-  Tabs, Tab, Input, Link, Button, Card, CardBody, CardHeader,
+  Tabs, Tab, Input, Link, Button,
 } from '@nextui-org/react';
 import { userLogin } from '../../controller/controller';
 
@@ -25,10 +25,13 @@ export default function Page() {
 
   const [selected, setSelected] = React.useState('login');
   const [loginError, setLoginError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const sendToServer = async (username, password) => {
+    setLoginError('');
+    setIsLoading(true);
     const response = await userLogin(username, password);
-
+    setIsLoading(false);
     if (response.ok) {
       const responseData = await response.json();
       localStorage.setItem('loginToken', responseData.token);
@@ -110,6 +113,28 @@ export default function Page() {
                     fullWidth
                     type="submit"
                     className="bg-red text-white hover:bg-red-hover"
+                    isLoading={isLoading}
+                    spinner={(
+                      <svg
+                        className="animate-spin h-5 w-5 text-current"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <circle
+                          className="opacity-25"
+                          cx="12"
+                          cy="12"
+                          r="10"
+                          stroke="currentColor"
+                          strokeWidth="4"
+                        />
+                        <path
+                          className="opacity-75"
+                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                    )}
                   >
                     Login
                   </Button>
