d23c98c61283595648148a8170aaa98bb7defe67
Siarhei Prakapovich <prokser27@icloud.com>
2023-11-12 00:25:53 +0300

    refactor: add breadcrumbs to profile page

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,9 +1,23 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
 
 export default function Profile() {
+  useEffect(() => {
+    // const userID = localStorage.getItem('')
+  }, []);
+
   return (
-    <div>Profile</div>
+    <>
+      <Breadcrumbs
+        itemCategory="Profile"
+      />
+      <main className="flex min-h-[666px] w-full relative flex-col items-center justify-center p-3">
+        <div className="w-full max-w-[1440px] flex justify-center items-center gap-4">
+          Profile
+        </div>
+      </main>
+    </>
   );
 }
