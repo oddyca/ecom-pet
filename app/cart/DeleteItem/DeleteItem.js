6d50e707ee9da392aea57eef070fe8aec887a5ab
Siarhei Prakapovich <prokser27@icloud.com>
2023-12-25 16:48:48 +0300

    fix: fix item deletion from cart

--- app/cart/DeleteItem/DeleteItem.js
+++ app/cart/DeleteItem/DeleteItem.js
@@ -5,13 +5,12 @@ import Image from 'next/image';
 import useStore from '../../../controller/store/store';
 
 export default function DeleteItem({ id, size }) {
-  const IS_LOGGED = localStorage.getItem('isLogged');
   const { removeFromCart } = useStore();
 
   return (
     <button
       type="button"
-      onClick={() => removeFromCart(id, IS_LOGGED, size)}
+      onClick={() => removeFromCart(id, size)}
       className="block grid place-content-center h-full w-[64px] bg-red rounded-lg justify-self-stretch place-self-stretch
        hover:bg-[#B94747]"
     >
